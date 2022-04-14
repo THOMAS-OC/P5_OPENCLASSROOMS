4ce62d4b63e58c21ca90fd7c5f5bee6d7dcb3971
= <=>
2022-04-14 16:27:29 +0000

    Algo panier en cours

---
+++ front/js/cart.js
@@ -0,0 +1,29 @@
+
+
+
+console.log(window.localStorage); // C'EST UN OBJET
+console.log(Object.keys(window.localStorage)); // liste des clefs du storage
+let listIdArticleWithColors = Object.keys(window.localStorage)
+let listIdArticle = []
+for (let idArticle of listIdArticleWithColors){
+    console.log(idArticle.split(" "));
+    listIdArticle.push(idArticle.split(" ")[0])
+}
+console.log(listIdArticle);
+
+// Création d'un nombre d'articles html égal au nombre d'élément dans le storage
+nbArticles = window.localStorage.length
+createIndex = 1
+const sectionArticle = document.querySelector("#cart__items");
+while (createIndex < nbArticles){
+    createIndex = createIndex + 1
+    let cloneArticle = document.querySelector(".cart__item").cloneNode(true)
+    sectionArticle.appendChild(cloneArticle)
+}
+
+/* 
+On parcours les identifiants des articles enregistrés
+pour chaque identifiant on travaille dans l'élément html article suivant,
+on insère le nom du produit, l'image de l'article, la couleur, le prix et la quantité
+*/
+
