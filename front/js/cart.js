de79dcba0473ae1dbb7e926afae4bc462fdd4e97
= <=>
2022-04-15 08:27:59 +0000

    Ecriture d'un script pour constituer le panier à partir des infos du storage

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,15 +1,17 @@
 
 
+// Consitution d'un tableau du panier
+let myCart = [];
 
-console.log(window.localStorage); // C'EST UN OBJET
-console.log(Object.keys(window.localStorage)); // liste des clefs du storage
-let listIdArticleWithColors = Object.keys(window.localStorage)
-let listIdArticle = []
-for (let idArticle of listIdArticleWithColors){
-    console.log(idArticle.split(" "));
-    listIdArticle.push(idArticle.split(" ")[0])
+for (let details of Object.keys(window.localStorage)){
+    productArray = [];
+    productArray.push(details.split(" ")[0]);
+    productArray.push(details.split(" ")[1]);
+    productArray.push(window.localStorage.getItem(details));
+    myCart.push(productArray);
 }
-console.log(listIdArticle);
+
+// On génère autant d'article html que le panier en contient
 
 // Création d'un nombre d'articles html égal au nombre d'élément dans le storage
 nbArticles = window.localStorage.length
@@ -21,22 +23,47 @@ while (createIndex < nbArticles){
     sectionArticle.appendChild(cloneArticle)
 }
 
-/* 
-On parcours les identifiants des articles enregistrés
-pour chaque identifiant on travaille dans l'élément html article suivant,
-on insère le nom du produit, l'image de l'article, la couleur, le prix et la quantité
-*/
+// On requête l'API pour obtenir les infos
+let i = 0 // variable de l'élément html "article" en cours !
+for (let info of myCart){
+    console.log(info[0]);
 
-let i = 0
+    fetch(`http://localhost:3000/api/products/${info[0]}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        const articleHTML = document.getElementsByTagName("article")
+        // data.price; // prix de l'article
+        articleHTML[i].querySelector("h2").innerText = data.name
+        // data.imageUrl // url de l'image
 
-for (let articlePanier of Object.values(window.localStorage)){
-
-    console.log(articlePanier);
-
-    const articleHTML = document.getElementsByTagName("article")
-
-    articleHTML[i].querySelector("input").value = articlePanier;
-
-    i ++
-
+        // console.log(info[1]); // Couleur du produit
+        // console.log(info[2]); // quantité de produit
+        articleHTML[i].querySelector("input").value = info[2]; // quantité de produit
+        i ++
+    });
 }
+
+
+
+
+
+// /* 
+// On parcours les identifiants des articles enregistrés
+// pour chaque identifiant on travaille dans l'élément html article suivant,
+// on insère le nom du produit, l'image de l'article, la couleur, le prix et la quantité
+// */
+
+// let i = 0
+
+// for (let articlePanier of Object.values(window.localStorage)){
+
+//     console.log(articlePanier);
+
+//     const articleHTML = document.getElementsByTagName("article")
+
+//     articleHTML[i].querySelector("input").value = articlePanier;
+
+//     i ++
+
+// }
