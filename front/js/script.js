ed95ed36256116a4f66d595cbc9dcd72c7b2fed6
= <=>
2022-06-07 19:56:59 +0000

    Remplacement de la boucle while pour la requete api inital par une boucle for of + moderne et + maintenable

--- front/js/script.js
+++ front/js/script.js
@@ -1,35 +1,34 @@
-let counter = 0
 const sectionArticles = document.querySelector(".items"); // Selection du parent
 
 fetch("http://localhost:3000/api/products")
     .then(res => res.json())
-    .then(data => {
+    .then(database => {
 
-        while(counter < data.length){
+
+        for(const data of database){
   
             let newProduct = document.createElement("a"); // Création d'un élément HTML
-            newProduct.href = `./product.html?id=${data[counter]._id}` // Ajout de l'attribut href
+            newProduct.href = `./product.html?id=${data._id}` // Ajout de l'attribut href
             sectionArticles.appendChild(newProduct); // Ajout de l'élément crée dans le parent
             
             let article = document.createElement("article");
             newProduct.appendChild(article);
             
             let articlePicture = document.createElement("img");
-            articlePicture.src = data[counter].imageUrl
-            articlePicture.alt = data[counter].altTxt
+            articlePicture.src = data.imageUrl
+            articlePicture.alt = data.altTxt
             
             let articleTitle = document.createElement("h3");
             articleTitle.className = "productName";
-            articleTitle.innerText = data[counter].name
+            articleTitle.innerText = data.name
             
             let articleDescription = document.createElement("p");
             articleDescription.className = "productDescription";
-            articleDescription.innerText = data[counter].description
+            articleDescription.innerText = data.description
             
             article.appendChild(articlePicture)
             article.appendChild(articleTitle)
             article.appendChild(articleDescription)
-            counter ++
 
         }
 
