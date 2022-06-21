25d441f7aef0b089df49683a1c33906949683c3b
= <=>
2022-06-21 13:33:53 +0000

    Insertion des produits directement via la syntaxe template litteral  et la propriété innerHTML, factorisation.

--- front/js/script.js
+++ front/js/script.js
@@ -12,24 +12,12 @@ fetch(`${URLCONST.URL_BASE}${URLCONST.ENDPOINT_GET}`)
             newProduct.href = `./product.html?id=${data._id}` // Ajout de l'attribut href
             sectionArticles.appendChild(newProduct); // Ajout de l'élément crée dans le parent
             
-            let article = document.createElement("article");
-            newProduct.appendChild(article);
-            
-            let articlePicture = document.createElement("img");
-            articlePicture.src = data.imageUrl
-            articlePicture.alt = data.altTxt
-            
-            let articleTitle = document.createElement("h3");
-            articleTitle.className = "productName";
-            articleTitle.innerText = data.name
-            
-            let articleDescription = document.createElement("p");
-            articleDescription.className = "productDescription";
-            articleDescription.innerText = data.description
-            
-            article.appendChild(articlePicture)
-            article.appendChild(articleTitle)
-            article.appendChild(articleDescription)
+            newProduct.innerHTML = 
+            `<article> 
+                <img src=${data.imageUrl} alt=${data.altTxt}>
+                <h3 class="productName"> ${data.name} </h3>
+                <p class="productDescription"> ${data.description} </p>
+            </article>`
 
         }
 
